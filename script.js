b31e93004294f97cbe7005081da40f45f1d19a72
GarbanzoBeanz <mhmoghaddassi@gmail.com>
2025-03-29 13:29:48 +0800

    Create a function to play a round and keep score
    This will enable the user to play one round against the computer and
    to keep score.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 'use strict'
 
-
 let randomNumber = getRandomInt();
+let humanScore = 0;
+let computerScore = 0;
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 function getRandomInt(max) {
   return Math.floor((Math.random() * 3) + 1)
@@ -22,4 +25,58 @@ function getComputerChoice() {
 function getHumanChoice() {
   let answer = prompt("Rock, Paper or Scissors?").toLowerCase();
   return answer;
+};
+
+function playRound(humanChoice, computerChoice) {
+  if(humanChoice == 'rock') {
+    switch (computerChoice) {
+      case 'Rock':
+        console.log('We both chose rock, we tie!');
+        break;
+      case 'Paper':
+        console.log('Paper beats rock, you lose!');
+        ++computerScore;
+        break;
+      case 'Scissors':
+        console.log('Rock beats scissors, you win!');
+        ++humanScore
+        break;
+      default:
+        console.log('Something went wrong!!');
+    };
+  } else if(humanChoice == 'paper') {
+    switch (computerChoice) {
+      case 'Rock':
+        console.log('Paper beats rock, you win!');
+        ++humanScore
+        break;
+      case 'Paper':
+        console.log('We both chose paper, we tie!');
+        break;
+      case 'Scissors':
+        console.log('Scissors beats paper, you lose!');
+        ++computerScore;
+        break;
+      default:
+        console.log('Something went wrong!!');
+    };
+  } else if(humanChoice == 'scissors') {
+      switch (computerChoice) {
+        case 'Rock':
+          console.log('Rock beats scissors, you lose!');
+          ++computerScore;
+          break;
+        case 'Paper':
+          console.log('Scissors beats paper, you win!');
+          ++humanScore
+          break;
+        case 'Scissors':
+          console.log('We both chose Scissors, we tie!');
+          break;
+        default:
+          console.log('Something went wrong!!');
+      };
+    } else {
+      console.log('something is broken');
+    }
 };
