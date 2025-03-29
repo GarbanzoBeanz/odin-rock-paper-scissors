ab80ecfad63be5f35d5c334fd90b3bcf0ab61d1e
GarbanzoBeanz <mhmoghaddassi@gmail.com>
2025-03-29 13:23:02 +0800

    Create the computer's selection function
    The computer's choice will be made using this function and our random
    number generator.

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function getRandomInt(max) {
   return Math.floor((Math.random() * 3) + 1)
 };
 
+function getComputerChoice() {
+  if(randomNumber == 1) {
+    return "Rock"
+  } else if(randomNumber == 2) {
+    return "Paper";
+  } else if (randomNumber == 3) {
+    return "Scissors";
+  } else {
+    return 'Something\'s Broken';
+  }
+};
