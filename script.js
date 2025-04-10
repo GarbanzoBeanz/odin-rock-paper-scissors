9fe1b614c778f346ec8fed9b91e3b7390ec9da6f
GarbanzoBeanz <150662238+GarbanzoBeanz@users.noreply.github.com>
2025-04-10 14:16:58 +0800

    Add game initializer and computer choice logic
    This sets up the base game state and randomness needed to make each
    round unpredictable and playable.

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ const mainScreen = document.querySelector('.main-container');
 
 // --- COMPONENT SELECTORS --- //
 const startBtn = document.querySelector('.start-btn');
+const choiceButtons = document.querySelectorAll('[data-choice]');
+
+const userScoreEl = document.querySelector('#user-score');
+const computerScoreEl = document.querySelector('#computer-score');
+
+// --- INITIALIZER --- //
+let userScore = 0;
+let computerScore = 0;
+userScoreEl.textContent = `Player Score: ${userScore}`;
+computerScoreEl.textContent = `Computer Score: ${computerScore}`;
 
 // --- EVENT LISTENERS --- //
 const startGameBtn = document.addEventListener('click', () => {
@@ -14,31 +24,24 @@ const startGameBtn = document.addEventListener('click', () => {
   mainScreen.classList.remove('hide');
 });
 
+// --- GAME FUNCTIONS --- //
+function getRandomInt(max) {
+  return Math.floor((Math.random() * 3) + 1);
+}
 
-// let humanScore = 0;
-// let computerScore = 0;
 
-// function getRandomInt(max) {
-//   return Math.floor((Math.random() * 3) + 1);
-// }
-
-// function getComputerChoice() {
-//   const randomNumber = getRandomInt();
-//   if(randomNumber == 1) {
-//     return "Rock";
-//   } else if(randomNumber == 2) {
-//     return "Paper";
-//   } else if (randomNumber == 3) {
-//     return "Scissors";
-//   } else {
-//     return 'Something\'s Broken';
-//   }
-// }
-
-// function getHumanChoice() {
-//   let answer = prompt("Rock, Paper or Scissors?").toLowerCase();
-//   return answer;
-// }
+function getComputerChoice() {
+  const randomNumber = getRandomInt();
+  if(randomNumber == 1) {
+    return "rock";
+  } else if(randomNumber == 2) {
+    return "paper";
+  } else if (randomNumber == 3) {
+    return "scissors";
+  } else {
+    return 'Somthing\'s Wrong!';
+  }
+}
 
 // function playRound(humanChoice, computerChoice) {
 //   if(humanChoice === 'rock') {
